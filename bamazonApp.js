75dbb9d25cc70b50d90cce2e5998bef5f048ca87
FedericoAlmenara <fnalmenaradumur96@gmail.com>
2019-01-26 11:07:44 -0500

    added images

--- bamazonApp.js
+++ bamazonApp.js
@@ -5,7 +5,7 @@ require("console.table");
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-    user:"nico",
+    user:"root",
     password:"Badgerman4161",
     database: "bamazon_db"
 
